cbd1ddabfcb4c40093a5e42f8e4977cb630d0947
Akash Patel <69709410+thefstalphabet@users.noreply.github.com>
2022-11-23 11:37:44 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000;
 
 app.get("/", (req, res) => {
-    return res.send("The API is running fine")
+    return res.send("Your simple node appp is running fine")
 });
 
 app.listen(port, () => console.log("Listening on port 3000"));
